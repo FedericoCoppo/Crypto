6bc731180f65f77ef0a7ef63b9c08ab2c8680dd6
FedericoCoppo <54506799+FedericoCoppo@users.noreply.github.com>
2019-08-25 22:37:37 +0200

    Update extractModExp.py
    key name provided via script input

--- rsaKeyCodeGenerator/extractModExp.py
+++ rsaKeyCodeGenerator/extractModExp.py
@@ -2,16 +2,24 @@
 import sys
 from Crypto.PublicKey import RSA
 
-pub_key = RSA.importKey(open("public_key.pem").read())
+keyFileName = sys.argv[1]
+pub_key = RSA.importKey(open(keyFileName).read())
+keyBitSize = (int(pub_key.size()) + 1)
+print keyBitSize
+if keyBitSize%512 != 0 :
+	print "Public_key.pem size error" 
+	exit()
 
 s = str(hex(pub_key.n))
-c = "const  uint8 modulus[MODULUS_BYTE_SIZE] =\n"
+c = "#define MODULUS_BYTE_SIZE " + str(keyBitSize/8)
+c += "\n\n"
+c += "const  uint8 modulus[MODULUS_BYTE_SIZE] =\n"
 c += "{ \n        "
 
 index = 0
 byte_num = 0
 
-while byte_num < 256 :
+while byte_num < keyBitSize/8 :
 	c += "0x"
 	c += s[index + 2].upper() + s[index + 2 + 1].upper() + 'u'
 	
@@ -29,10 +37,11 @@ while byte_num < 256 :
 c += "};\n"	
 with open('KeyModule.c', 'wb') as f:
     f.write(c)	
-print "Generating Cal_Module.c file containing modulus:" 	
+print "Generating KeyModule.c file containing modulus:" 	
 print c
 
-c = "const  uint8 exponent[EXPONENT_BYTE_SIZE] = { "
+c = "#define EXPONENT_BYTE_SIZE 3 \n\n"
+c += "const  uint8 exponent[EXPONENT_BYTE_SIZE] = { "
 s = str(hex(pub_key.e))
 
 # exponent management
@@ -56,7 +65,7 @@ c += " };"
 with open('KeyExponent.c', 'wb') as f:
     f.write(c)	
 print "Generating KeyExponent.c file containing exponent:" 	
-print c 
+print c
 
 
 
