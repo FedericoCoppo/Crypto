f70f68c0b5fc5822f024cb8e5863e644a721a1af
FedericoCoppo <54506799+FedericoCoppo@users.noreply.github.com>
2019-11-02 11:21:18 +0100

    Delete GenSignature.sh

--- signatureTool/GenSignature.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-echo '****starting sign  ******'
-cp file.bin fileSigned.bin
-binaryfile=fileSigned.bin
-openssl dgst -sha256 -sign private.pem -out sign $binaryfile
-cat sign >> $binaryfile
-rm sign
-echo '**** file signed  ******'
